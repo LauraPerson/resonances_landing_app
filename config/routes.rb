26a0ee6116b5b556a73de5ca0367b0fbd288285c
laura person <lauraperson29800@gmail.com>
2022-08-16 12:23:45 +0200

    devise, cloudinary, dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :dashboard, :controller => 'users', only: [:show]
+  resources :dashboard, :controller => 'users', only: [:show, :edit]
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
