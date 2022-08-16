6a4fcda90b9d1013b761e732e6ef5f23e7c665bc
laura person <lauraperson29800@gmail.com>
2022-08-16 17:18:50 +0200

    pundit

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,37 +1,46 @@
 class ClientsController < ApplicationController
     def index
-      @clients = Client.all
+      @clients = policy_scope(Client.all)
     end
   
     def show
       @client = Client.find(params[:id])
+      authorize @client
     end
   
     def new 
       #current_user.super_admin?
       @client = Client.new
+      authorize @client
+
     end 
   
     def create
       @client = Client.new(client_params)
       @client.save 
+      authorize @client
+
       flash.alert = "Nouveau client ajouté"
       redirect_to clients_path
     end
   
     def edit
       @client = Client.find(params[:id])
+      authorize @client
+
     end
   
     def update
       @client = Client.find(params[:id])
       @client.update(client_params)
+      authorize @client
       redirect_to clients_path
     end
   
     def destroy
       @client = Client.find(params[:id])
       @client.destroy
+      authorize @client
       flash.alert = "Client supprimé"
       redirect_to clients_path
     end
