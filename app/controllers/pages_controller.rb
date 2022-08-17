ed4e68195742c479868c89a67a3310490b88a12f
laura person <lauraperson29800@gmail.com>
2022-08-17 17:28:34 +0200

    added workers to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,6 @@ class PagesController < ApplicationController
   def home
     @clients = Client.all
     @last_articles = Article.all.sort_by{|e| e[:created_at]}.last(3)
+    @workers = Worker.all
   end
 end
