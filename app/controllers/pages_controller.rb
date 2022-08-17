0666b326cf8254d75e5023f901997a850b354951
laura person <lauraperson29800@gmail.com>
2022-08-17 12:45:48 +0200

    added element in home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @clients = Client.all
+    @last_articles = Article.all.sort_by{|e| e[:created_at]}.last(3)
   end
 end
